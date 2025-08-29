88a76c7c2869d7bb204a537c025671a268056d3e
Michelle777-1 <michelleaqmaqm3@gmail.com>
2025-08-29 12:46:08 -0500

    Update Pruebas.py

--- Pruebas.py
+++ Pruebas.py
@@ -1,1 +1,10 @@
 #Pruebas
+#En el archivo debe de estar:
+#Descripcion de las actividades
+#Ejercio 1 manejar error ejem si se divide por 0 entonces que mande error
+#numeros amigos, raices, seno, multiplicacion dos valores 
+#Describir como va a hacer las funciones 
+#Caratula todos los integrantes
+#Anexos con el repositorio y enlace del github para que lo veo
+#Diagramas? o simplemente para explicar nuestro codigo 
+#
