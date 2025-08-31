a16e8740662221246ebc4c54063becd2718b7cea
Michelle <michelleaqmaqm3@gmail.com>
2025-08-31 19:35:33 +0000

    actividad2prueba

--- Actividad2/funcionesIngreso.py
+++ Actividad2/funcionesIngreso.py
@@ -2,4 +2,4 @@ def es_un_numero(n):
     if n.isdigit():
         print("Ingresó un número:",n)
     else:
-        pritn("Error, no ingresó un número:",n)
+        print("Error, no ingresó un número:",n)
