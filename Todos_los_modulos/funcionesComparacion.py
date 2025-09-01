8095a2043564bd8dbf5526125dbd90982a3813b3
Naomm0828 <naomi612186@gmail.com>
2025-09-01 18:02:18 -0500

    módulo a

---
+++ Todos_los_modulos/funcionesComparacion.py
@@ -0,0 +1,18 @@
+def maximo(val1,val2):
+    if val1>val2:
+        max=val1
+    else:
+        max=val2
+    print("El valor máximo es: ",max)
+def minimo(val1,val2):
+    if val1<val2:
+        min=val1
+    else:
+        min=val2
+    print("El valor mínimo es: ",min)
+def iguales(val1,val2):
+    print("¿Los números son iguales?")
+    return val1==val2
+def diferentes(val1,val2):
+    print("¿Los números son diferentes?")
+    return val1!=val2
