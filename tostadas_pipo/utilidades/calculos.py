ab594578d14a8c3b4d9614ba912e29b9e46c8905
Naomm0828 <naomi612186@gmail.com>
2025-09-01 20:43:21 -0500

    Add files via upload

---
+++ tostadas_pipo/utilidades/calculos.py
@@ -0,0 +1,4 @@
+def suma_total(monto=0):
+    calculo_suma = 20
+    calculo_suma += monto
+    return calculo_suma
---
+++ tostadas_pipo/utilidades/impuestos.py
@@ -0,0 +1,6 @@
+def impuesto_iva12(monto=0):
+    total = ((monto * 12)/100)
+    return total
+def impuesto_iva14(monto=0):
+    total=((monto * 14)/100)
+    return total
