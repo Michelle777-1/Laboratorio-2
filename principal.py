a6efb45151604a525df5b25a174888cab5ccb631
Naomm0828 <naomi612186@gmail.com>
2025-09-01 20:30:22 -0500

    Delete principal.py

--- principal.py
+++
@@ -1,7 +0,0 @@
-from Todos_los_modulos.utilidades import calculos
-from Todos_los_modulos.utilidades import impuestos
-
-monto = int(input("Introduzca un monto entero:"))
-print("El impuesto IVA de 12%:", impuestos.impuesto_iva12(monto))
-suma = int(input("Introduzca un monto entero a sumar:"))
-print("La suma total es:", calculos.suma_total(suma))
