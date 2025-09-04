26597ceaf9a1f4983a9a6db84ec1f59fbb6aeef1
Naomm0828 <naomi612186@gmail.com>
2025-09-03 22:52:24 -0500

    Update chats.py

--- Whatsapp/Módulos/chats.py
+++ Whatsapp/Módulos/chats.py
@@ -1,1 +1,26 @@
-
+from .mensajes import enviar_msj, ver_mensajes
+from .Contactos import contacto, lista_contactos
+def iniciar_chat():
+    if not contacto:
+        print("No tiene contactos, agrega uno")
+        return
+    lista_contactos()
+    nombre=input("Escriba el nombre del contacto con el que quiere iniciar un chat: ")
+    if nombre not in contacto:
+        print("Ese contacto no existe")
+        return
+    while True:
+        print("---Chat con ",nombre"---")
+        print("1 Enviar mensaje")
+        print("2 Ver mensajes")
+        print("3 Volver al menú")
+        opcion=int(input("Seleccione opción: "))
+        if opcion==1:
+            texto=input("Escriba su mensaje: ")
+            enviar_msj(nombre,texto)
+        elif opcion==2:
+            ver_mensajes(nombre)
+        elif opcion==3:
+            break
+        else:
+            print ("Opción no válida")
