049e71bb46916d74ad309850da9a193b98ef508f
Naomm0828 <naomi612186@gmail.com>
2025-09-03 22:12:09 -0500

    Update mensajes.py

--- Whatsapp/Módulos/mensajes.py
+++ Whatsapp/Módulos/mensajes.py
@@ -1,1 +1,14 @@
+mensajes={}
+def enviar_msj(contacto, texto):
+    if contacto not in mensajes:
+        mensajes[contacto]=[]
+    mensajes[contacto].append(texto)
+    print("Mensaje enviado a ",contacto," : ",texto)
 
+def ver_mensajes(contacto):
+    if contacto not in mensajes or not mensajes[contacto]:
+        print("No tiene mensajes con ",contacto)
+    else:
+        print("Chat con ",contacto,":" )
+        for i in mensajes[contacto]:
+            print("-",i)
