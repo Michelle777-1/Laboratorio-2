cbd7b370c8bb3e963b9f6a7761355747090b42ea
Michelle <michelleaqmaqm3@gmail.com>
2025-09-04 01:17:18 +0000

    avances

---
+++ Todos_los_modulos/funciones_calculadora.py
@@ -0,0 +1,41 @@
+import math
+def suma():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def resta():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    resta = a-b
+    return resta
+def división():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def multiplicación():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def potencia():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def raiz():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def factorial():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
+def conversion():
+    a = float(input("Ingrese número:"))
+    b = float(input("Ingrese número:"))
+    suma = a+b
+    return suma
--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,35 @@
-from Todos_los_modulos import Funciones_calculadora
+from Todos_los_modulos import funciones_calculadora
 opcion = 1
-while opcion != 6:
-    def mostrar_menu():
+while opcion != 9:
     print("MENÚ DE OPCIONES")
     print("1. Sumar")
     print("2. Restar")
     print("3. Dividir")
     print("4. Multiplicar")
     print("5. Potencia")
-    print("6. Raiz")
+    print("6. Raíz")
     print("7. Factorial")
     print("8. Conversion (radiones y grados)")
+    print("9. Salir")
 
     opcion = int(input("Seleccione una opción: "))
     if opcion == 1:
-        
+        print("La suma es", funciones_calculadora.suma())
     elif opcion == 2:
-        
+        print("La resta es", funciones_calculadora.resta())
     elif opcion == 3:
-        
+        print("La dvisión es", funciones_calculadora.división())
     elif opcion == 4:
-        
+        print("La multiplicación es", funciones_calculadora.multiplicación())
     elif opcion == 5:
-        
+        print("La potencia es", funciones_calculadora.potencia())
     elif opcion == 6:
+        print("La raíz es", funciones_calculadora.raiz())
+    elif opcion == 7:
+        print("El factorial es", funciones_calculadora.factorial())
+    elif opcion == 8:
+        print("La conversión es", funciones_calculadora.conversion())
+    elif opcion == 9:
         print("Fin")
     else:
         print("Opción no válida")
