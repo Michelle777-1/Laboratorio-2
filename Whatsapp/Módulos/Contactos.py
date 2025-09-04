36d3ae39c2bbcb66bde3510f75498432258ba920
Michelle <michelleaqmaqm3@gmail.com>
2025-09-04 03:24:45 +0000

    contacto

--- Whatsapp/Módulos/Contactos.py
+++ Whatsapp/Módulos/Contactos.py
@@ -1,19 +1,19 @@
-contactos = {}
+contacto = {}
 def agregar_contacto():
     nombre = input("Ingrese nombre del nuevo contacto:")
     numero = input("Ingrese número del contacto:")
     if len(numero) <9:
         numero = input("Agrege número de telefono completo")
-    contactos[nombre] = int(numero)
+    contacto[nombre] = int(numero)
     return print ("Contacto agregado")
 def eliminar_contacto():
     nombre = input("Ingrese nombre del contacto ha eliminar:")
-    if nombre in contactos:
-        contactos.pop[nombre]
+    if nombre in contacto:
+        contacto.pop[nombre]
     else:
         print("Ese contacto no existe")
 def lista_contactos():
     print("--------Contactos--------")
-    for nombre,numero in contactos.items():
+    for nombre,numero in contacto.items():
         print(f"{nombre}: {numero}")
     
