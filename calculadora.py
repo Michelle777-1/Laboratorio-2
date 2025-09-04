1da3ecae064629ca51a1806b45de30d6aed0485f
Michelle <michelleaqmaqm3@gmail.com>
2025-09-04 00:58:03 +0000

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+from Todos_los_modulos import Funciones_calculadora
+opcion = 1
+while opcion != 6:
+    def mostrar_menu():
+    print("MENÚ DE OPCIONES")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Dividir")
+    print("4. Multiplicar")
+    print("5. Potencia")
+    print("6. Raiz")
+    print("7. Factorial")
+    print("8. Conversion (radiones y grados)")
+
+    opcion = int(input("Seleccione una opción: "))
+    if opcion == 1:
+        
+    elif opcion == 2:
+        
+    elif opcion == 3:
+        
+    elif opcion == 4:
+        
+    elif opcion == 5:
+        
+    elif opcion == 6:
+        print("Fin")
+    else:
+        print("Opción no válida")
